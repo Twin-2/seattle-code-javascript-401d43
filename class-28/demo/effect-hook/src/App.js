6b7845c1e36c3d3771f7786709a381f69585589c
John Cokos <highlander.44@gmail.com>
2021-09-22 12:49:32 -0700

    Demo for day 28

---
+++ class-28/demo/effect-hook/src/App.js
@@ -0,0 +1,23 @@
+import {useState, useEffect} from 'react';
+
+import People from './People.js';
+
+function App() {
+
+  const [person, setPerson] = useState("");
+  const [heading, setHeading] = useState("");
+
+  // This could be looking at the request object ...
+  useEffect( () => {
+    setHeading( person.toUpperCase());
+  }, [person])
+
+  return (
+    <div className="App">
+        <h2>{heading}</h2>
+        <People onSubmit={setPerson} />
+    </div>
+  );
+}
+
+export default App;
